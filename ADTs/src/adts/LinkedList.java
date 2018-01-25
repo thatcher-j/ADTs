e7fe70463dea5c166a816bd690a494e17aa86336
thatcher-j <thatcher-j@1W07-11.uwds.uwinnipeg.ca>
2018-01-25 10:53:45 -0600

    Added proper commenting.

--- ADTs/src/adts/LinkedList.java
+++ ADTs/src/adts/LinkedList.java
@@ -1,10 +1,10 @@
 /* **********************************************************
- * Programmer:	Rob Sveinson
- * Class:		CS20S
+ * Programmer:	Joshua R. Thatcher
+ * Class:		CS40S
  * 
- * Assignment:	aX  qY
+ * Assignment:	ADTs
  *
- * Description:	describe the class you are creating
+ * Description:	LinkedList data type class
  *
  * 
  * *************************************************************
@@ -25,29 +25,29 @@ package adts;
  	// ********** constructors ***********
         public LinkedList ()
         {
-            head = new Node ();
-            current = head;
+            head = new Node (); //create head using default constructor
+            current = head; //set the current node to the head
         }
  	
  	// ********** accessors **********
-        public Object get ()
+        public Object get () //return the current node's payload
         {
             return current.getData ();
         }
         
-        public void find (Object o)
+        public void find (Object o) //automatically traverse the list to go directly to the node with a given payload
         {
-            if (current.getData() != o)
+            if (current.getData() != o) //if the payload isn't contained in the current node
             {
                 Node temp = current;
                 current = head.getPointer ();
 
-                while ((current.getData () != o) && (current.getPointer () != null))
+                while ((current.getData () != o) && (current.getPointer () != null)) //while the payload isn't contained in the current node and it isn't the last node in the list
                 {
                     current = current.getPointer();
                 }
 
-                if (current.getData () != o)
+                if (current.getData () != o) //if the last element was reached but didn't have the given payload
                 {
                     System.out.println ("Could not find.");
                     current = temp;
@@ -55,15 +55,15 @@ package adts;
             }
         }
         
-        public boolean isEmpty ()
+        public boolean isEmpty () //if the list is empty
         {
             return (head.getPointer () == null);
         }
  	
  	// ********** mutators **********
-        public void next ()
+        public void next () //advances the current node by one
         {
-            if (current.getPointer () != null)
+            if (current.getPointer () != null) //if not at the end of the list
             {
                 current = current.getPointer ();
             }
@@ -72,27 +72,27 @@ package adts;
                 System.out.println ("End of list reached. Please reset.");
             }
         }
-        public void reset ()
+        public void reset () //move the pointer back to the beginning
         {
             current = head.getPointer();
         }
         
-        public void insert (Object o)
+        public void insert (Object o) //insert a new piece of data at the current location
         {
             Node temp = current;
             current = new Node (o, current.getPointer ());
             temp.editPointer (current);
         }
         
-        public void insertFirst (Object o)
+        public void insertFirst (Object o) //insert a new piece of data at the start of the list
         {
             Node temp = new Node (o, head.getPointer ());
             head.editPointer(temp);
         }
         
-        public void insertLast (Object o)
+        public void insertLast (Object o) //insert a new piece of data at the end of the list
         {
-            while (current.getPointer () != null)
+            while (current.getPointer () != null) //while not at the last element
             {
                 current = current.getPointer ();
             }
@@ -102,12 +102,12 @@ package adts;
             temp.editPointer (current);
         }
         
-        public void delete ()
+        public void delete () //delete the current piece of data
         {
             Node temp = current;
             current = head.getPointer();
             
-            while (current.getPointer () != temp)
+            while (current.getPointer () != temp) //while not at the previous node in the list
             {
                 current = current.getPointer ();
             }
@@ -115,7 +115,7 @@ package adts;
             current.editPointer (temp.getPointer ());
         }
         
-        public void deleteFirst ()
+        public void deleteFirst () //delete the first piece of data
         {
             Node temp = head.getPointer();
             temp = temp.getPointer();
@@ -124,24 +124,22 @@ package adts;
             current = temp;
         }
         
-        public void deleteLast ()
+        public void deleteLast () //delete the last piece of data
         {
-            //Node temp = current;
             Node prev = current;
             
             if (current.getPointer () == null) //if currently on last element
             {
-                //Node prev = current;
                 current = head.getPointer();
                 
-                while (current.getPointer () != prev)
+                while (current.getPointer () != prev) //while not at the element previous to last element
                 {
                     current = current.getPointer ();
                 }
             }
             else //if not
             {
-                while (current.getPointer () != null)
+                while (current.getPointer () != null) //while not at the end of the list
                 {
                     prev = current;
                     current = current.getPointer ();
@@ -149,7 +147,6 @@ package adts;
             }
             
             prev.editPointer (null);
-            //current = temp;
             current = prev;
         }
         
@@ -159,38 +156,38 @@ package adts;
  	// *********** class constants **********
  	
  	// ********** instance variable **********
-            Node pointer;
-            Object data;
+            Node pointer; //next node object in the list
+            Object data; //payload
  	
  	// ********** constructors ***********
-            private Node ()
+            private Node () //for the head
             {
                 data = "***HEAD***";
             }
             
-            private Node (Object o, Node n)
+            private Node (Object o, Node n) //for all others
             {
                 pointer = n;
                 data = o;
             }
  	
  	// ********** accessors **********
-            private Object getData ()
+            private Object getData () //return payload
             {
                 return data;
             }
             
-            private Node getPointer ()
+            private Node getPointer () //return next node in the list
             {
                 return pointer;
             }
  	
  	// ********** mutators **********
-            private void editData (Object o)
+            private void editData (Object o) //edit payload
             {
                 data = o;
             }
-            private void editPointer (Node n)
+            private void editPointer (Node n) //edit the pointer
             {
                 pointer = n;
             }
